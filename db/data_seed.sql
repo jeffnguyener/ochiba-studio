48d1b5d83d1e0676985fd7d8a4d78be6a0e86f11
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-04 11:35:30 -0600

    data seed adjusted

--- db/data_seed.sql
+++ db/data_seed.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users {
-  user_id INTEGER SERIAL PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   phone INTEGER,
@@ -9,12 +9,13 @@ CREATE TABLE users {
 }
 
 CREATE TABLE shopping_cart {
-  id INTEGER,
-  user_id INTEGER
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(user_id)
 }
 
 CREATE TABLE item {
-  cart_id INTEGER,
+  id SERIAL PRIMARY KEY,
+  cart_id INTEGER REFERENCES shopping_cart(id),
   item_name VARCHAR(50),
   description VARCHAR(200),
   quantity INTEGER,
@@ -22,22 +23,28 @@ CREATE TABLE item {
 }
 
 CREATE TABLE products {
-  product_id INTEGER SERIAL PRIMARY KEY,
-  product_name VARCHAR(60),
+  product_id SERIAL PRIMARY KEY,
+  product_name VARCHAR(50),
   product_description VARCHAR(200),
   price INTEGER
 }
 
 CREATE TABLE gallery {
-  user_id INTEGER,
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(user_id),
   image TEXT,
-  image_id INTEGER SERIAL PRIMARY KEY
+}
+
+CREATE TABLE images {
+    img_id SERIAL PRIMARY KEY,
+    img TEXT,
+    gallery_id INTEGER REFERENCES gallery(id)
 }
 
 CREATE TABLE messages {
   id INTEGER.
-  sender_id INTEGER,
-  recepient_id INTEGER,
+  sender_id INTEGER REFERENCES users(user_id),
+  recepient_id INTEGER REFERENCES users(user_id),
   body_msg VARCHAR(500),
   date DATETIME
 }
