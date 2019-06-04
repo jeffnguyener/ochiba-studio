c46dcf4b8ee23aa2e989310eabc9341f42e3d156
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-04 11:42:18 -0600

    fixed data seed

--- db/data_seed.sql
+++ db/data_seed.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users {
+CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
@@ -6,45 +6,45 @@ CREATE TABLE users {
   email VARCHAR(100),
   password TEXT,
   avatar TEXT
-}
+);
 
-CREATE TABLE shopping_cart {
+CREATE TABLE shopping_cart (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id)
-}
+);
 
-CREATE TABLE item {
+CREATE TABLE item (
   id SERIAL PRIMARY KEY,
   cart_id INTEGER REFERENCES shopping_cart(id),
   item_name VARCHAR(50),
   description VARCHAR(200),
   quantity INTEGER,
   price INTEGER
-}
+);
 
-CREATE TABLE products {
+CREATE TABLE products (
   product_id SERIAL PRIMARY KEY,
   product_name VARCHAR(50),
   product_description VARCHAR(200),
   price INTEGER
-}
+);
 
-CREATE TABLE gallery {
+CREATE TABLE gallery (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id),
-  image TEXT,
-}
+  image TEXT
+);
 
-CREATE TABLE images {
+CREATE TABLE images (
     img_id SERIAL PRIMARY KEY,
     img TEXT,
     gallery_id INTEGER REFERENCES gallery(id)
-}
+);
 
-CREATE TABLE messages {
-  id INTEGER.
+CREATE TABLE messages (
+  id INTEGER,
   sender_id INTEGER REFERENCES users(user_id),
   recepient_id INTEGER REFERENCES users(user_id),
   body_msg VARCHAR(500),
-  date DATETIME
-}
+  date TEXT
+);
