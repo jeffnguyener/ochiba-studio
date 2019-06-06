abf453a63899852bce55ccb37a62225bbc5b0e7e
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-06 13:32:53 -0600

    add color on links with hover

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = ({ email }) => {
             <div>
             LOGO
             </div>
-            <div>
+            <div className="links">
             <Link to='/'>Home</Link>
             <Link to='/login'>Login</Link>
             <Link to='/aboutus'>About Us</Link>
