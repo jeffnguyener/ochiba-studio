b1324b5018e36c4a426523552ec88d6588647224
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-08 22:23:03 -0600

    update navbar added shop ctrl & sql

---
+++ server/controllers/shopping_controller.js
@@ -0,0 +1,9 @@
+module.exports = {
+    getItems: (req, res) => {
+        const db = req.app.get('db');
+        db.get_all_products()
+            .then(response => {
+                res.status(200).send(response)
+            })
+    }
+}
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,81 +1,82 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import * as Icon from 'react-feather'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as Icon from "react-feather";
 
-import oslogo from './images/oslogo.png'
-import './Navbar.css'
+import oslogo from "./images/oslogo.png";
+import "./Navbar.css";
 
 class Navbar extends Component {
-    constructor() {
-        super()
-        this.state = {
-            showDropdown: false,
-            showLoginDropdown: false,
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      showDropdown: false,
+      showLoginDropdown: false
+    };
+  }
 
-    toggleDropdown = () => {
-        this.setState({ showDropdown: !this.state.showDropdown })
-    }
+  toggleDropdown = () => {
+    this.setState({ showDropdown: !this.state.showDropdown });
+  };
 
-    toggleLoginDropdown = () => {
-        this.setState({
-            showLoginDropdown:
-                !this.state.showLoginDropdown
-        })
-    }
+  toggleLoginDropdown = () => {
+    this.setState({
+      showLoginDropdown: !this.state.showLoginDropdown
+    });
+  };
 
-    render() {
-        return (
-            <nav className="navbar">
-                <div>
-                    <img src={oslogo}></img>
-                </div>
-                <div className="links">
-                    <Link to='/'>Home</Link>
-                    <Link to='/aboutus'>About Us</Link>
-                    <span onClick={this.toggleDropdown}>Portfolio</span>
-                    <Link to='/pricing'>Pricing</Link>
-                    <Link to='/blog'>Blog</Link>
-                    <Link to="/cart">
-                    <Icon.ShoppingCart size={28} className="cart" />
-                    </Link>
-                    <Icon.Menu size={30} className="hamburger" onClick={this.toggleLoginDropdown} />
+  render() {
+    return (
+      <nav className="navbar">
+        <div>
+          <img src={oslogo} />
+        </div>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/aboutus">About Us</Link>
+          <span className="p-dropdown" onClick={this.toggleDropdown}>
+            Portfolio
+            {this.state.showDropdown ? (
+              <div className="port-dropdown">
+               <div className="divider"/>
+                <Link to="#">Wedding</Link>
+                <div className="divider"/>
+                <Link to="#">Engagement</Link>
+                <div className="divider"/>
+                <Link to="#">Portraits</Link>
+                <div className="divider"/>
+                <Link to="#">Maternity</Link>
+              </div>
+            ) : null}
+          </span>
+          <Link to="/pricing">Pricing</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/cart">
+            <Icon.ShoppingCart size={28} className="cart" />
+          </Link>
+          <Icon.Menu
+            size={30}
+            className="hamburger"
+            onClick={this.toggleLoginDropdown}
+          />
+        </div>
+        {/* login/register drop down */}
+        {this.state.showLoginDropdown ? (
+          <div className="sidebar">
+            <Link to="/login">Login/Register</Link>
+            <Link to="/cart">Shopping Cart</Link>
+          </div>
+        ) : null}
 
-                </div>
-                {/* login/register drop down */}
-                {
-                    this.state.showLoginDropdown ?
-                        <div className="sidebar">
-                            <Link to='/login'>Login/Register</Link>
-                            <Link to='/cart'>Shopping Cart</Link>
-                        </div>
-                        :
-                        null
-                }
-
-                {/* products drop down */}
-                {
-                    this.state.showDropdown ?
-                        <div className="port-dropdown">
-                            <Link to='#'>Wedding</Link>
-                            <Link to='#'>Engagement</Link>
-                            <Link to='#'>Portraits</Link>
-                            <Link to='#'>Maternity</Link>
-                        </div>
-                        :
-                        null
-                }
-
-            </nav>
-        )
-    }
+        {/* products drop down */}
+      </nav>
+    );
+  }
 }
 
-const mapStateToProps = (reduxState) => {
-    return {
-        email: reduxState.email
-    }
-}
-export default connect(mapStateToProps)(Navbar)
+const mapStateToProps = reduxState => {
+  return {
+    email: reduxState.email
+  };
+};
+export default connect(mapStateToProps)(Navbar);
