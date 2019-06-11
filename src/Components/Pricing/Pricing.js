95876dd7de52bc9de591e4092a96d694266965a5
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-11 16:42:30 -0600

    update pricing & shopping

--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -1,41 +1,67 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { connect } from 'react-redux'
-import { addToCart } from '../../redux/shoppingReducer';
+import React, { Component } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { addToCart } from "../../redux/shoppingReducer";
+import styled from "styled-components";
+
+import "./Pricing.css";
+
+const Button = styled.button`
+  font-family: sans-serif;
+  font-size: 16px;
+  border: none;
+  padding: 3px 8px;
+  background: lightgray;
+  color: white;
+
+  &:hover {
+    background: black;
+    cursor: pointer;
+  }
+`;
 
 class Pricing extends Component {
-    state = {
-        products: [],
-        selectedPackage: {}
-    }
+  state = {
+    products: [],
+    selectedPackage: {}
+  };
 
-    componentDidMount(){
-        axios.get('/shopping/items')
-        .then((res)=> this.setState({products: res.data}))
-    }
+  componentDidMount() {
+    axios
+      .get("/shopping/items")
+      .then(res => this.setState({ products: res.data }));
+  }
 
-    handleAddItem = (id) => {
-        console.log('the id is', id)
-    }
+  handleAddItem = id => {
+    console.log("the id is", id);
+  };
 
-    render(){
-       const items = this.state.products.map(product => {
-           return (
-               <div key={product.product_id}>
-                   <h1>Package: {product.product_name}</h1>
-                   <h5>Description: {product.product_description} </h5>
-                   <h5>Price: {product.price}</h5>
-                   <button onClick={() => this.props.addToCart(product)}>Add To Cart</button>
-               </div>
-           )
-       })
+  render() {
+    const items = this.state.products.map(product => {
+      return (
+        <div key={product.product_id}>
+          <h1>Package: {product.product_name}</h1>
+          <h5>Description: {product.product_description} </h5>
+          <h4>Price: ${product.price}</h4>
+          <Button onClick={() => this.props.addToCart(product)}>
+            Add To Cart
+          </Button>
+          <span>
+            <br />
+            <br />
+          </span>
+          <div className="break" />
+        </div>
+      );
+    });
 
-        return (
-            <>
-                {items}
-            </>
-        )
-    }
+    return <div><h1 className="pricing">Pricing</h1>
+    <div className="pricing-form">{items}</div>
+    </div>;
+  }
 }
 
-export default connect(null, {addToCart})(Pricing);
+export default connect(
+  null,
+  { addToCart }
+)(Pricing);
--- src/Components/Shopping/Shopping.js
+++ src/Components/Shopping/Shopping.js
@@ -1,31 +1,65 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import { clearCart } from "../../redux/shoppingReducer";
 
+import "./Shopping.css";
+
+const Button = styled.button`
+  font-family: sans-serif;
+  font-size: 16px;
+  border: none;
+  padding: 3px 8px;
+  background: lightgray;
+  color: white;
+
+  &:hover {
+    background: black;
+    cursor: pointer;
+  }
+`;
+
+// handleClearCart = (id) => {
+//     const { id } = this.props.cart
+//     this.props.handleClearCart
+// }
 
 class Shopping extends Component {
-    render() {
-        const items = this.props.cart.map(product => {
-            return (
-                <div key={product.product_id}>
-                    <h1>Package: {product.product_name}</h1>
-                    <h5>Description: {product.product_description} </h5>
-                    <h5>Price: {product.price}</h5>
-                    <button>Remove</button>
-                </div>
-            )
-        })
-        return (
-            <div>
-                {items}
-            </div>
-        )
-    }
+  render() {
+    const items = this.props.cart.map(product => {
+      return (
+          <div key={product.product_id}>
+            <h2>Package: {product.product_name}</h2>
+            <h5>Description:</h5> {product.product_description} 
+            <h4>Price: ${product.price}</h4>
+            <Button onClick={this.handleClearCart}>Remove</Button>
+            <span>
+                <br /><br />
+            </span>
+            <div className="break"></div>
+          </div>
+      );
+    });
+    return <div><h1 className="shoppingcart">Shopping Cart</h1>
+    <div className="cart-form">
+    {items}
+    </div>
+    </div>
+    ;
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        cart: state.shoppingReducer.cart
-    }
+function mapStateToProps(reduxState) {
+  return {
+    cart: reduxState.shoppingReducer.cart
+  };
 }
 
-export default connect(mapStateToProps)(Shopping);
+const mapDispatchToProps = {
+  clearCart
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Shopping);
