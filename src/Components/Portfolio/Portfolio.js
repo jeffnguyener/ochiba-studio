1aec52ced7b2cbbe656db4c5dbb09d969b3818da
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-12 16:23:40 -0600

    portfolio adjusted

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import './Portfolio.css'
+
 class Portfolio extends Component{
     render(){
         return(
-            <div>
-                <Link to='/'>Wedding</Link>
-                <Link to='/'>Engagement</Link>
-                <Link to='/'>Portraits</Link>
-                <Link to='/'>Maternity</Link>
+            <div className="box-container">
+                <Link to='/wedding'>Wedding</Link>
+                <Link to='/engagement'>Engagement</Link>
+                <Link to='/portraits'>Portraits</Link>
+                <Link to='/maternity'>Maternity</Link>
             </div>
         )
     }
