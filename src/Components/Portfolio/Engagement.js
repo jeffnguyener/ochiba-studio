d1ad4c644aa379e4cfe554c356717efe7207fdfa
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-13 14:31:14 -0600

    AWS not working, had to local host imgs

--- src/Components/Portfolio/Engagement.js
+++ src/Components/Portfolio/Engagement.js
@@ -3,22 +3,37 @@ import Gallery from "react-waterfall-gallery";
 
 import './Portfolio.css'
 
-const engage_set = [
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/20170610-DSC_1527.jpg",
-    "https://live.staticflickr.com/4240/34964846280_a0c10d4368_k.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/20170610-DSC_1649-2.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_5833.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_6372.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_6410.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_6448.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_6453.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/IMG_6762.jpg"
-];
+import eng1 from './Engagement/engage1.jpg'
+import eng2 from './Engagement/engage2.jpg'
+import eng3 from './Engagement/engage3.jpg'
+import eng4 from './Engagement/engage4.jpg'
+import eng5 from './Engagement/engage5.jpg'
+import eng6 from './Engagement/engage6.jpg'
+import eng7 from './Engagement/engage7.jpg'
+import eng8 from './Engagement/engage8.jpg'
+
+
+const engage_img = [ "https://live.staticflickr.com/4240/34964846280_a0c10d4368_k.jpg", eng1, eng2, eng3, eng4, eng5, eng6, eng7, eng8 ];
+
+// const engage_set = [
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/20170610-DSC_1527.jpg",
+//     "https://live.staticflickr.com/4240/34964846280_a0c10d4368_k.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/20170610-DSC_1649-2.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_5833.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_6372.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_6410.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_6448.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/DSC_6453.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Engagement/IMG_6762.jpg"
+// ];
 
 export default class Engagement extends Component {
   render() {
     return <div className="box-container">
-      <Gallery step={100} images={engage_set} /> 
+      <Gallery step={100} 
+      // images={engage_set} 
+      images={engage_img}
+      /> 
       </div>
   }
 }
--- src/Components/Portfolio/Portraits.js
+++ src/Components/Portfolio/Portraits.js
@@ -3,23 +3,40 @@ import Gallery from "react-waterfall-gallery";
 
 import './Portfolio.css'
 
-const portrait_set = [
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/DSC_3201.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/DSC_3314-2.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/choly.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/de.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/father-son.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/game-day.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/rashedia.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/sergio.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/valentine.jpg",
-  "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/zander.jpg"
-];
+import port1 from './Portraits/portrait1.jpg'
+import port2 from './Portraits/portrait2.jpg'
+import port3 from './Portraits/portrait3.jpg'
+import port4 from './Portraits/portrait4.jpg'
+import port5 from './Portraits/portrait5.jpg'
+import port6 from './Portraits/portrait6.jpg'
+import port7 from './Portraits/portrait7.jpg'
+import port8 from './Portraits/portrait8.jpg'
+import port9 from './Portraits/portrait9.jpg'
+import port10 from './Portraits/portrait10.jpg'
+
+
+const portrait_img = [ port1, port2, port3, port4, port5, port6, port7, port8, port9, port10 ];
+
+// const portrait_set = [
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/DSC_3201.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/DSC_3314-2.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/choly.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/de.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/father-son.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/game-day.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/rashedia.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/sergio.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/valentine.jpg",
+//   "https://os-personal-project.s3-us-west-1.amazonaws.com/Portraits/zander.jpg"
+// ];
 
 export default class Portraits extends Component {
   render() {
     return <div className="box-container">
-      <Gallery step={100} images={portrait_set} /> 
+      <Gallery step={100} 
+      // images={portrait_set} 
+      images={portrait_img}
+      /> 
       </div>
   }
 }
