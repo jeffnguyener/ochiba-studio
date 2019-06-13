e97d738da253389e6a67fcde38b9a419c196194b
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-13 14:14:53 -0600

    AWS not working on campus, local hosting imgs

--- src/Components/Portfolio/Maternity.js
+++ src/Components/Portfolio/Maternity.js
@@ -3,20 +3,34 @@ import Gallery from "react-waterfall-gallery";
 
 import './Portfolio.css'
 
-const maternity_set = [
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160526-DSC_0717.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160526-DSC_0722.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160605-DSC_0764.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160605-DSC_0790.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160605-DSC_0820.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/DSC_5520-2.jpg",
-    "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/DSC_5605-2.jpg"
+import mat1 from './Maternity/maternity1.jpg'
+import mat2 from './Maternity/maternity2.jpg'
+import mat3 from './Maternity/maternity3.jpg'
+import mat4 from './Maternity/maternity4.jpg'
+import mat5 from './Maternity/maternity5.jpg'
+import mat6 from './Maternity/maternity6.jpg'
+import mat7 from './Maternity/maternity7.jpg'
+
+const maternity_img = [ mat1, mat2, mat3, mat4, mat5, mat6, mat7
 ];
+
+// const maternity_set = [
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160526-DSC_0717.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160526-DSC_0722.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160605-DSC_0764.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160605-DSC_0790.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/20160605-DSC_0820.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/DSC_5520-2.jpg",
+//     "https://os-personal-project.s3-us-west-1.amazonaws.com/Maternity/DSC_5605-2.jpg"
+// ];
 
 export default class Maternity extends Component {
   render() {
     return <div className="box-container">
-      <Gallery step={100} images={maternity_set} /> 
+      <Gallery step={100} 
+      // images={maternity_set} 
+      images={maternity_img}
+      /> 
       </div>
   }
 }
