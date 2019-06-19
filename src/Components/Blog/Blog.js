e5e2ceb308af789c31ba7461565141a0ecf6b786
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-18 22:47:42 -0600

    update blog with skydive

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import styled from "styled-components";
 
 import parisImg from "../Navbar/images/paris.jpg";
+import skydiveUt from '../Navbar/images/skydive_ut.jpg'
+
 import "./Blog.css";
 
 const Image = styled.img`
@@ -19,6 +21,13 @@ const Blog = () => (
       </div>
       <br />
     <span className="blog-post"><h1>Feb 2019</h1>We traveled to Paris! Of course, we had to do a photoshoot with the Eiffel Tower in the background. It's definitely bigger than the one in Las Vegas! Lol, it is truly a sight to see. European architecture is amazing and with so much history behind them. We would travel back again, just to see more history throughout the country. 
+    </span>
+    <br />
+    <div className="zoomIn">
+      <Image id="skydive" alt="skydive" src={skydiveUt} />
+      </div>
+      <br />
+    <span className="blog-post"><h1>Sept 2018</h1>Crossed this off my bucket list, thanks to my wife for this awesome early birthday gift. What an experience this was! If you haven't tried it, you have to! I would definitely do this again. Thank you Skydive Utah for a great first time experience. They sure take care of you and prep you for this. 
     </span>
     <br />
     <br />
